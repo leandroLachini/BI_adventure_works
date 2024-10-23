8b2c071283f051cd59249baf5d2a93201d715eb1
LEANDRO LACHINI NASCIMENTO <leandrolachini@gmail.com>
2024-10-23 19:48:25 +0000

    Conexão com a tabela SALESORDERDETAIL

---
+++ models/staging/erp/stg_erp__SALESORDERDETAIL.sql
@@ -0,0 +1,26 @@
+/* Conexão com a fonte dos pedidos vendidos detalhados */ 
+
+with
+    source_salesorderdetail as (
+        select 
+        *
+        from {{ source('erp_adventure_works', 'SALESORDERDETAIL') }}
+    )
+
+/* Renomeando colunas da tabela e categorizando os dados */
+
+    , remane_table as (
+        select
+        md5(SALESORDERID) as SK_SALESORDERDETAIL
+        , cast(SALESORDERID as int) as PK_ORDERID
+        , cast(SALESORDERDETAILID as int) as PK_ORDERDETAILID
+        , cast(PRODUCTID as int) as FK_PRODUCTID
+        , cast(SPECIALOFFERID as int) as FK_SPECIALOFFERID
+        , cast(ORDERQTY as int) as QUANTITY
+        , cast(UNITPRICE as numeric(18,2)) as UNIT_PRICE
+        , cast(UNITPRICEDISCOUNT as float) as UNITPRICEDISCOUNT
+        from source_salesorderdetail
+    )
+
+select *
+from remane_table
