c62310ddfcad32427e03b289b759855d28b7c4ad
LEANDRO LACHINI NASCIMENTO <leandrolachini@gmail.com>
2024-10-23 21:13:46 +0000

    Criado a dimensao CUSTOMER.

---
+++ models/sales_department/intermediate/int_customer_joins.sql
@@ -0,0 +1,32 @@
+/* Conexao com a staging clientes */
+
+with
+    customer as (
+        select
+        *
+        from {{ ref("stg_erp__CUSTOMER")}}
+    )
+
+/* Conexao com a staging pessoas */
+    
+    , person as (
+        select
+        *
+        from {{ ref("stg_erp__PERSON")}}
+    )
+
+/* Fazendo os joins para popular tabela com dados relevantes */
+
+    , joined as (
+        select
+        customer.PK_CUSTOMERID
+        , person.PK_PERSONID
+        , customer.FK_STOREID
+        , customer.FK_TERRITORYID
+        , person.PERSON_FULL_NAME
+        from customer
+        left join person on person.PK_PERSONID = customer.FK_PERSONID
+    )
+select
+*
+from joined
