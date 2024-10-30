3bdbc2b29c7fef149e74dbce65d15dca8d8d418f
LEANDRO LACHINI NASCIMENTO <leandrolachini@gmail.com>
2024-10-30 17:55:57 +0000

    Join com SalesReason

--- macros/sales_gross_income.sql
+++ macros/sales_gross_income.sql
@@ -5,3 +5,15 @@
 {% macro net_value(column_1, column_2, column_3) %}
     ({{column_1}} * {{column_2}} * (1 - {{column_3}}))::numeric(16, 4) as NET_VALUE
 {% endmacro %}
+
+{% macro select_reason(number_reason) %}
+    (
+        select
+            PK_SALESORDERID
+            , FK_SALESREASONID
+        from {{ref("stg_erp__SALESORDERHEADERSALESREASON")}}
+        where
+            1 = 1
+            and FK_SALESREASONID = {{number_reason}}
+    )
+{% endmacro %}
